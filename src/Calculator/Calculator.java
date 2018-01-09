b2ce41ffcd941e180cb14a8f83ba7b49118c290f
DESKTOP-K2J3TU6\Jorge <jorge.fernandez107@alu.ulpgc.es>
2018-01-09 19:29:20 +0000

    Kata String Calculator finished.

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,51 @@
+package Calculator;
+
+public class Calculator {
+
+    private final String delimiter = ",|\n";
+
+    public int calculate(String input) throws Exception {
+
+        String[] numbers = input.split(delimiter);
+        if (isEmpty(input)) {
+            return 0;
+        }
+        if (input.length() == 1) {
+            return stringToInt(input);
+        } else {
+            return getSum(numbers);
+        }
+    }
+
+    private int getSum(String[] numbers) throws Exception {
+        findDagerousInput(numbers);
+        return calculateValues(numbers);
+    }
+
+    private int calculateValues(String[] numbers) {
+        int sum = 0;
+        for (String current : numbers) {
+            if (stringToInt(current) > 1000) {
+                continue;
+            }
+            sum += stringToInt(current);
+        }
+        return sum;
+    }
+
+    private void findDagerousInput(String[] numbers) throws Exception {
+        for (String current : numbers) {
+            if (stringToInt(current) < 0) {
+                throw new Exception("Negative input");
+            }
+        }
+    }
+
+    private boolean isEmpty(String input) {
+        return input.isEmpty();
+    }
+
+    private int stringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+}
